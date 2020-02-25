a7c342f9d0d9d19d458a3c224b62f47b7d55076e
thanhnguyenhu66 <thanh.nguyenhuu66@gmail.com>
2020-02-25 12:13:31 +0000

    Create new pakage and class for power ups

---
+++ core/src/com/dicycat/kroy/powers/DoubleDamage.java
@@ -0,0 +1,17 @@
+package com.dicycat.kroy.powers;
+
+import com.dicycat.kroy.GameObject;
+
+
+/**
+ *
+ * Double damage
+ *
+ * @author Thanh
+ */
+public class DoubleDamage extends GameObject {
+
+    @Override
+    public void update() {
+    }
+}
